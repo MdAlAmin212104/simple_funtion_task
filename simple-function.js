9a1a387b029c68f987c1efde8be5748027459295
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-02-03 11:07:01 +0600

    fineal problem solved

--- simple-function.js
+++ simple-function.js
@@ -62,4 +62,10 @@ console.log(count_zero("01010111000110100000"));
 ### Task-5 
 Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return `Even`. If odd return `Odd`
 */
-
+function odd_even(integer){
+      if(integer %2 == 0){
+            return "Even";
+      }
+      return "Odd";
+}
+console.log(odd_even(24));
